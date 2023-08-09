f47e380870f6fe9d2a25a50401f6422afa12b07c
p-gatsby <106583795+p-gatsby@users.noreply.github.com>
2023-08-09 03:38:23 -0700

    added initial frontend

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Navbar, Nav, Container, Row } from "react-bootstrap";
+
+function Header() {
+  return (
+    <div>
+      <Navbar expand="lg" bg="dark" variant="dark" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="#home">PROSHOP</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+              <Nav.Link href="/login"><i className="fas fa-user"></i> Login</Nav.Link>
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
