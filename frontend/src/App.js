4bf75f73a3bb13c53505355156e9059e3b2def68
p-gatsby <106583795+p-gatsby@users.noreply.github.com>
2023-08-09 03:38:23 -0700

    feat: react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header.js"
+import Footer from "./components/Footer.js"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Welcome</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
