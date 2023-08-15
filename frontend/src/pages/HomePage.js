3163c2f65772e312dd4c1e009a92c291a9ffa6b9
p-gatsby <106583795+p-gatsby@users.noreply.github.com>
2023-08-15 01:15:29 -0700

    feat: product and homepage components

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+import products from "../products.js";
+import Product from "../components/Product.js";
+
+function HomePage() {
+  return (
+    <div>
+      {" "}
+      <h1>Latest Products </h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+}
+
+export default HomePage;
