c42523ac79dae26d25a8a9b1bc8481713518e1da
p-gatsby <106583795+p-gatsby@users.noreply.github.com>
2023-08-15 01:15:29 -0700

    added product and homepage components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
+import { Container } from 'react-bootstrap';
+
 import Header from "./components/Header.js"
 import Footer from "./components/Footer.js"
+
+import HomePage from './pages/HomePage.js';
 
 function App() {
   return (
     <div>
       <Header />
-      <main>
-        <h1>Welcome</h1>
+      <main className="py-3">
+        <Container>
+          <HomePage />
+        </Container>
       </main>
       <Footer />
     </div>
