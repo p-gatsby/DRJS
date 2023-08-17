6403efc39344d0f71411adf296b8f9527f40fc7e
p-gatsby <106583795+p-gatsby@users.noreply.github.com>
2023-08-17 01:40:47 -0700

    feat: routing with react-router-dom and productpage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { Container } from 'react-bootstrap';
+import {Routes, Route} from 'react-router-dom';
 
 import Header from "./components/Header.js"
 import Footer from "./components/Footer.js"
 
 import HomePage from './pages/HomePage.js';
+import ProductPage from './pages/ProductPage.js';
 
 function App() {
   return (
@@ -11,7 +13,10 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <HomePage />
+          <Routes>
+            <Route  path='/' element={<HomePage />} exact />
+            <Route path='/product/:id' element={<ProductPage />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
