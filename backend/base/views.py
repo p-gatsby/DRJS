ce074e83deaec64a559588f3e989174ce05b0f64
p-gatsby <106583795+p-gatsby@users.noreply.github.com>
2024-01-20 15:47:18 -0800

    refactor: import paths and remove unused URLs

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,26 +4,20 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Product
-from .serializers import ProductSerializer 
+from .serializers import ProductSerializer
 
 # Create your views here.
 
 
-def getRoutes(request):
-    routes = [
-        ...
-    ]
-    return JsonResponse('Hello', safe=False)
-
 @api_view(['GET'])
 def getProducts(request):
     products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True) 
+    serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
- 
-@api_view(['GET']) 
+
+
+@api_view(['GET'])
 def getProduct(request, pk):
     product = Product.objects.get(id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
-  
