f64290bb1dfc6dc03098dd10d29a53bef570a857
p-gatsby <106583795+p-gatsby@users.noreply.github.com>
2024-06-14 18:16:23 -0700

    feat: add items to cart

--- Client/src/reducers/cartReducers.js
+++ Client/src/reducers/cartReducers.js
@@ -1,17 +1,37 @@
-import {
-  createAction,
-  createReducer,
-  createAsyncThunk,
-} from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const CART_ADD_ITEM = createAction("CART_ADD_ITEM");
+export const CART_ADD_ITEM = "CART_ADD_ITEM";
 
-const CART_REMOVE_ITEM = createAction("CART_REMOVE_ITEM");
+const CART_REMOVE_ITEM = "CART_REMOVE_ITEM";
+
+const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`);
+
+  // console.log(data)
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data.id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
 
 export const cartReducer = createReducer(
   {
-    cartItems: [],
+    cartItems: cartItemsFromLocalStorage,
   },
   (builder) => {
     builder.addCase(CART_ADD_ITEM, (state, action) => {
